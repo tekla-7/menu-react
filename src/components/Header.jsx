7a0ad80cedfd2c7f2132ef64a0bf6546dd2f9d88
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-06-13 12:13:42 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logoImg from '../assets/logo.jpg'
+export default function Header() {
+  return (
+    <header id='main-header'>
+      <div id="title">
+        <img src={logoImg} alt="logo img" />
+        <h1>Food</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
