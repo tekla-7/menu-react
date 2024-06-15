65d7174276745b26c00be05cee20a8022dd1aff8
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-06-15 11:47:06 +0400

    end

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,8 +5,16 @@ import { currencyFormatter } from "../util/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import useHttp from "./hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig={
+method:'POST',
+headers:{'Content-Type':'applicaion/json'}
+}
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
+  const {data,isLoading , error ,isSending , sendRequest}=useHttp('http://localhost:3000/orders' ,requestConfig);
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
@@ -19,19 +27,23 @@ export default function Checkout() {
     event.preventDefault();
    const fd= new FormData(event.target)
    const customerData=Object.fromEntries(fd.entries())
-
-   fetch('http://localhost:3000/orders',{
-    method:'POST' ,
-    headers:{
-        'Content-Type' : 'application/json'
-    },
-    body:JSON.stringify({
-        order:{
-            items:cartCtx.items,
-            customer:customerData 
-        }
-    })
-   })
+sendRequest(JSON.stringify({
+  order:{
+      items:cartCtx.items,
+      customer:customerData 
+  }
+}))
+  }
+  let actions=(
+    <>
+    <Button type="button" textOnly onClick={handleClose}>
+            Close{" "}
+          </Button>
+          <Button>Submit Order </Button>
+          </>
+  )
+  if(isSending){
+    actions=<span>Sending order data ...</span>
   }
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
@@ -45,11 +57,9 @@ export default function Checkout() {
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
+        {error && <Error title='failed to submit' message={error} />}
         <p className="modal-actions">
-          <Button type="button" textOnly onClick={handleClose}>
-            Close{" "}
-          </Button>
-          <Button>Submit Order </Button>
+          {actions}
         </p>
       </form>
     </Modal>
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,20 +1,17 @@
 import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
-
+import useHttp from "../components/hooks/useHttp"
+import Error from "./Error";
+const requestConfig={}
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-        console.log("error");
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+ const {data:loadedMeals , isLoading ,error}=useHttp('http://localhost:3000/meals' , requestConfig,[])
 
+if(isLoading){
+  return <p className="center">Fetching ...</p>
+}
+if(error){
+  return <Error title='gailed to fetch' message={error}></Error>
+}
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => {
